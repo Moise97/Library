2e39880515889636053d1c0cfa53f67ba28befe0
Solcan Moise <solcan_moise2014@yahoo.com>
2019-04-27 23:40:19 +0300

    Create header component

--- src/header/header.module.js
+++ src/header/header.module.js
@@ -1,13 +1,17 @@
 let headerModule = angular.module('headerModule', []);
 
 headerModule.controller('myHeaderController', ['$scope', function($scope){
-    $scope.masa = 'Home';
+    $scope.buttonName1 = "Home";
+    $scope.buttonName2 = "Books";
+    $scope.buttonName3 = "Ebooks";
+    $scope.buttonName4 = "News";
+    $scope.buttonName5 = "About";
+    $scope.buttonName6 = "Contact";
 }]);
 
 headerModule.directive('myHeaderDirective', function(){
     return {
         restrict: 'EA',
-        templateUrl: 'src/header/header.html',
         templateUrl: 'src/header/header.html',
         scope: false
     };
